8f2eb7b9ebdd968b4428f4b2e5ff2413ce577f4a
Eric Ferguson <ferguson.eric.r@gmail.com>
2020-09-08 12:29:38 -0700

    def draw

--- main.py
+++ main.py
@@ -80,7 +80,11 @@ class snake(object):
         pass
 
     def draw(self, surface):
-        pass
+        for i, c in enumerate(self.body):
+            if i == 0:
+                c.draw(surface, True)
+            else:
+                c.draw(surface)
 
 def drawGrid(w, rows, surface):
     sizeBtwn = w // rows
