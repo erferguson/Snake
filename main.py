7555c12d7dabba5faf3dcd1e7db6a9947e8b253b
Eric Ferguson <ferguson.eric.r@gmail.com>
2020-09-08 11:30:03 -0700

    created > def main(), def redrawWindow(), def drawGrid

--- main.py
+++ main.py
@@ -36,9 +36,24 @@ class snake(object):
         pass
 
 def drawGrid(w, rows, surface):
+    sizeBtwn = w // rows
+
+    x = 0
+    y = 0
+    for l in range(rows):
+        x = x + sizeBtwn
+        y = y + sizeBtwn
+
+        pygame.draw.line(surface, (255,255,255), (x,0), (x,w))
+        pygame.draw.line(surface, (255,255,255), (0,y), (w,y))
+
     pass
 
 def redrawWindow(surface):
+    global rows, width
+    win.fill((0,0,0))
+    drawGrid(width, row, surface)
+    pygame.display.update()
     pass
 
 def randomSnack(rows, items):
@@ -48,14 +63,23 @@ def message_box(subject, content):
     pass
 
 def main():
+    global width, rows
+    width = 500
+    rows = 20
+    win = pygame.display.set_mode((width, width))
+
+    s = snake((255,0,0), (10,10))
+
+    flag = True
+    clock = pygame.time.Clock() # controls speed of the game, ?
+
+    while flag:
+        pygame.time.delay(50)
+        clock.tick(10)
+
+        redrawWindow(win)
     pass
 
-rows = 
-w =
-h = 
-
-cube.rows = rows
-cube.w = w
 
 main()
 
