a0c0f3aa10e2cad9675be11f37b4999f58fc0f8c
Eric Ferguson <ferguson.eric.r@gmail.com>
2020-09-08 12:27:40 -0700

    def move >> else: movement of head

--- main.py
+++ main.py
@@ -65,6 +65,13 @@ class snake(object):
                 c.move(turn[0], turn[1])
                 if i == len(self.body) - 1:
                     self.turns.pop()
+            
+            else: 
+                if c.dirnx == -1 and c.pos[0] <= 0: c.pos = (c.rows-1, c.pos[1])
+                elif c.dirnx == 1 and c.pos[0] >= c.rows - 1: c.pos = (0,c.pos[1])
+                elif c.dirny == 1 and c.pos[1] >= c.rows - 1: c.pos = (c.pos[0],0)
+                elif c.dirny == -1 and c.pos[1] <= 0: c.pos = (c.pos[0], c.row-1)
+                else: c.move(c.dirnx, c.dirny)
 
     def reset(self, pos):
         pass
