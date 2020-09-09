2aa16a9e50949004cbe37757eb11807ba12c57c1
Eric Ferguson <ferguson.eric.r@gmail.com>
2020-09-09 11:11:51 -0700

    eyes for snake > created

--- main.py
+++ main.py
@@ -7,8 +7,8 @@ import tkinter as tk
 from tkinter import messagebox
 
 class cube(object):
-    rows = 0
-    w = 0
+    rows = 20
+    w = 500
 
     def __init__(self, start, dirnx=1, dirny=0, color=(255,0,0)):
         self.pos = start
@@ -22,7 +22,20 @@ class cube(object):
         self.pos(self.pos[0] + self.dirnx, self.pos[1] + self.dirny)
 
     def draw(self, surface, eyes=False):
-        pass
+        dis = self.w // self.rows
+        i = self.pos[0] # row
+        j = self.pos[1] # column
+
+        pygame.draw.rect(surface, self.color, (i*dis+1, j*dis+1, dis-2, dis-2))
+        if eyes:
+            centre = dis//2
+            radius = 3
+            circleMiddle = (i*dis + centre - radius, j*dis+8)
+            circleMiddle2 = (i*dis + dis - radius*2, j*dis+8)
+            pygame.draw.circle(surface, (0,0,0), circleMiddle, radius)
+            pygame.draw.circle(surface, (0,0,0), circleMiddle2, radius)
+            
+
 
 class snake(object):
     body = []
